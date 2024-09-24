a88fbee5f198074131688158466199e2622ad3cf
Ethan Lynch <ethanlynch72@gmail.com>
2024-09-23 21:22:52 -0400

    portfolio update

--- frontend/src/components/ProjectData.jsx
+++ frontend/src/components/ProjectData.jsx
@@ -1,20 +1,11 @@
 import pokemonthumbnail from '../media/pokemonthumbnail.png';
 import thumbnail from '../media/thumbnail.png';
-import work from '../media/work.gif';
+import puddleJumpersvid from '../media/puddle.gif'
 
 
 export const ProjectData = [
   {
     id: 1,
-    title: "Ethan Lynch Coaching",
-    description: "An intuitive and interactive coaching platform designed to empower users to enhance their skills and achieve personal growth through tailored coaching sessions and resources",
-    technologies: ['React', 'Django', 'Tailwind'],
-    imageUrl: work,
-    githubUrl: "https://github.com/XEthanLynchX/ethan-lynch",
-    liveDemoUrl: "https://ethan-lynch-coaching.web.app/",
-  },
-  {
-    id: 2,
     title: "Pokemon Handheld",
     description: "A user-friendly and intuitive Pokemon management application allowing users to organize and interact with their collection of captured Pokemon effectively.",
     technologies: ['React', 'Axios', 'Flask', 'MySQL'],
@@ -23,12 +14,20 @@ export const ProjectData = [
     liveDemoUrl: "https://www.youtube.com/watch?v=YjYFz6mf0vU&t=3s",
   },
   {
-    id: 3,
+    id: 2,
     title: "MoveMetrics",
     description: "An Application that allows users to log and track their workouts. In order to keep track of progression throughout their fitness journey.",
     technologies: ['React', 'Node.js', 'Express', 'MongoDB'],
     imageUrl: thumbnail,
     githubUrl: "https://github.com/XEthanLynchX/MoveMetrics",
     liveDemoUrl: "https://www.youtube.com/watch?v=Bx8snIJxfA0",
+  },
+  {
+    id: 3,
+    title: "PuddleJumpers",
+    description: "Puddle Jumpers is a adorable children’s store in downtown Exeter, New Hampshire, offering unique and stylish baby clothes and kids' apparel since 1989.",
+    technologies: ['Figma', 'Liquid', 'Shopify'],
+    imageUrl: puddleJumpersvid,
+    liveDemoUrl: "https://puddlejumpersnh.com/",
   },
 ];
